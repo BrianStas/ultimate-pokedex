8ead402b66d58085af7a534baf0017c5770ea4d6
Brian Stasiukaitis <brianstas@gmail.com>
2024-02-07 12:25:06 -0500

    fixed landing page
    Made the landing page look better with styled buttons. Added in colors for the theme in Tailwind config file, so my color scheme should now work throughout the site.

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     
     
-    <main>
-    <Link href="/Pokedex" className=''>Browse by Pokedex</Link>
-    <Link href="/Pokemon" className=''>Browse by Type</Link>
-    <Link href="/Pokemon" className=''>Browse Alphabetically</Link>
+    <main className = "grid grid-cols-3 gap-6 grid-flow-col ">
+    <Link href="/Pokedex" className='bg-marian-blue hover:bg-blue-500 text-white font-bold py-2 px-4 rounded text-center'>Browse by Pokedex</Link>
+    <Link href="/Pokemon" className='bg-flourescent-cyan hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded text-center'>Browse by Type</Link>
+    <Link href="/Pokemon" className='bg-kelly-green hover:bg-green-600 text-white font-bold py-2 px-4 rounded text-center'>Browse Alphabetically</Link>
     
     </main>
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'kelly-green': '#29BF12',
+        'onyx': '#313638',
+        'marian-blue': '#414288',
+        'tomato': '#F95738',
+        'flourescent-cyan': '#30F2F2',
+      }
     },
   },
   plugins: [require("daisyui")],
