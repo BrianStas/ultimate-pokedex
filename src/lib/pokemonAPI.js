c8e095121db98c82cf9054f786c5720e9a5d8dbb
Brian Stasiukaitis <brianstas@gmail.com>
2024-03-20 23:59:06 -0400

    another todo
    reminder of what work needs to go into the API component so I can call for the different materials

--- src/lib/pokemonAPI.js
+++ src/lib/pokemonAPI.js
@@ -1,6 +1,8 @@
 
 const POKEMON_API = "https://pokeapi.co/api/v2/";
 
+// TODO: get request for moves? Potentially get request for abilities?
+
 export async function getPokemonList(){
     const response = await fetch(POKEMON_API + "pokemon?limit=50&offset=0");
     const data = await response.json();
