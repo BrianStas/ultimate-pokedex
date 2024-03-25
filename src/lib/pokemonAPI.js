3aea6a24d1ef3ee2cd530c1596076edf3a6d435d
Brian Stasiukaitis <brianstas@gmail.com>
2024-03-24 21:37:17 -0400

    expanded api file
    added api listing for the Full list which would include all pokemon and their individual cards.

--- src/lib/pokemonAPI.js
+++ src/lib/pokemonAPI.js
@@ -8,3 +8,20 @@ export async function getPokemonList(){
     const data = await response.json();
     return data.results;
 }
+export async function getFullPokemonList(){ 
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=50/')
+      .then((res) => res.json())
+      .then((res) => {
+        const monList = res.results;
+        let promises = monList.map((pokemon) => {
+          return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+        });
+        Promise.all(promises)
+          .then((res) => Promise.all(res.map((r) => r.json())))
+          .then((res) => {
+            setPokeList(res);
+            setLoading(false);
+          });
+      })
+      .catch((e) => console.log(e));
+    }
