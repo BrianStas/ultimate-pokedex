5139cfaecc5e2532bc8c0ecdeb198fe35fffc29c
Brian Stasiukaitis <brianstas@gmail.com>
2024-04-19 18:11:40 -0400

    refactored api call
    Refactored api call to use async await. Currently not called in main file, so will need to export and then use once again to test functionality.

--- src/lib/pokemonAPI.js
+++ src/lib/pokemonAPI.js
@@ -8,20 +8,22 @@ export async function getPokemonList(){
     const data = await response.json();
     return data.results;
 }
-export async function getFullPokemonList(){ 
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=50/')
-      .then((res) => res.json())
-      .then((res) => {
-        const monList = res.results;
-        let promises = monList.map((pokemon) => {
-          return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-        });
-        Promise.all(promises)
-          .then((res) => Promise.all(res.map((r) => r.json())))
-          .then((res) => {
-            setPokeList(res);
-            setLoading(false);
-          });
-      })
-      .catch((e) => console.log(e));
+export async function getFullPokemonList(){
+  try{
+  const url = 'https://pokeapi.co/api/v2/pokemon'
+  const response = await fetch(url)
+  const data = response.json()
+  const monList = data.results;
+  let promises = monList.map((pokemon) => {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+  });
+  Promise.all(promises)
+    .then((res) => Promise.all(res.map((r) => r.json())))
+    .then((res) => {
+      setPokeList(res);
+      setLoading(false);
+    });
+  }
+  catch(e){
+    console.log(e)};
     }
